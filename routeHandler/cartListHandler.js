2f4328f59897873f91d1af9066c3b068040a9a87
Kakon Ray <thisiskakonray@gmail.com>
2022-06-22 18:42:34 +0600

    cartlist completed

--- routeHandler/cartListHandler.js
+++ routeHandler/cartListHandler.js
@@ -16,6 +16,7 @@ router.get("/", async (req, res) => {
   res.send(allValues);
 });
 
+// Post cartlist
 router.post("/", async (req, res) => {
   const data = req.body;
   let result = await cartCollection.insertOne(data);
